cf55af9499d62020c9d7c45cc63411cb22bd91c0
Metin Alim <metinalim@Metins-MacBook-Pro.local>
2024-08-14 03:55:05 +0300

    Add mockData and switch feature in api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,16 +1,24 @@
 import axios from 'axios';
 import { Candle } from '../redux/cryptoSlice';
 import { minimizeCandlesData } from '../helpers/commonHelpers';
+import cryptos from './mockData/symbols.json';
+import klines from './mockData/klines.json';
 
 const access_key = '6163179b2f3AswGRx0OKM8699'
 const API_BASE_URL = `http://localhost:7391/api/`;
+const useMockData = true;
 
 export const listCryptos = async () => {
   try {
-    // console.log("listCryptos")
-    const response = await axios.get(`${API_BASE_URL}/symbols?access_key=${access_key}`);
+    let response
+    if (useMockData) {
+      const randomList = [...cryptos.data]
+      // randomize mocked cryptos array each time
+      response = { data: { data: randomList.sort(() => Math.random() - 0.5) } }
+    } else {
+      response = await axios.get(`${API_BASE_URL}/symbols?access_key=${access_key}`);
+    }
     const { data } = response.data
-    // console.log("api ls data", (data.length));
     return data;
   } catch (error) {
     console.error(error)
@@ -20,17 +28,20 @@ export const listCryptos = async () => {
 
 export const getCrypto = async (symbol: string, minimizeLimit = 100): Promise<Candle[]> => {
   try {
-    // console.log("getCrypto", symbol)
+    let response
     const url = `${API_BASE_URL}/klines?access_key=${access_key}`
     // 90 days before
     let fromDate = new Date().getTime();
     fromDate = fromDate - 120 * 24 * 60 * 60 * 1000
     fromDate = fromDate / 1000
-    symbol = `${symbol}USDT`
-    const response = await axios.get(url, { params: { symbol, fromDate } });
-    // console.log("response", response);
+    if (useMockData) {
+      // @ts-ignore mock data is only for demo purpose, avoiding additional interface ...
+      response = { data: { data: klines[symbol].data } }
+    } else {
+      symbol = `${symbol}USDT`
+      response = await axios.get(url, { params: { symbol, fromDate } });
+    }
     let { data } = response.data
-    // console.log("data", data)
     if (data.length > minimizeLimit) {
       data = minimizeCandlesData(data as Candle[]);
     }
